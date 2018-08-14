612781320022bc55614dd9cbbc604aec0e02b6ec
Johknotter Hill <johknotterhill@gmail.com>
2018-08-14 16:55:18 -0400

    Restructured folders after completing the state, reducers and actions of the context api created

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
 import Contact from "./Contact";
-import { Consumer } from '../context';
+import { Consumer } from '../../context';
 
 
 class Contacts extends Component {
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-
-    const newContacts = contacts.filter(contact => contact.id !== id);
-
-    this.setState({
-      contacts: newContacts
-    })
-
-  };
-
   render() {
     return (
       <Consumer>
@@ -27,7 +15,6 @@ class Contacts extends Component {
                 <Contact
                   key={contact.id}
                   contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
                 />
               )}
             </React.Fragment>
